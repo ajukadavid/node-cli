87ec5637c2ce48e17643c916b348ac146776d344
ithiel883 <ajuka.zephiniah@gmail.com>
2023-09-13 14:54:45 +0100

    connected cli to terminal

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 #!/usr/bin/env node
+const note = process.argv[2]
+console.log(process.argv)
 
-console.log('b')
+//process.argv is an array with 3 values, first value is the environment, second is the file and the third thing is the argument passed when calling the command
+const newNote = {
+    content: note,
+    id: Date.now()
+}
+
+console.log(newNote)
