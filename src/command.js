e30fd3e60a506b8fe6f02d15ab6f04cceabd6cc4
ithiel883 <ajuka.zephiniah@gmail.com>
2023-09-19 18:43:32 +0100

    getting commands from terminal

--- src/command.js
+++ src/command.js
@@ -2,11 +2,13 @@ import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
 
 yargs(hideBin(process.argv))
-    .command('new <note>', 'Create a new note', (yargs) => {}, () => {
-        return yargs.positional('note', {
-          type: 'string',
-          description: 'The note to create'
-        })
+    .command('new <note>', 'Create a new note', (yargs) => {
+      return yargs.positional('note', {
+        type: 'string',
+        description: 'The note to create'
+      })
+    }, (argv) => {
+       console.log(argv.note)
     })    
 
   .demandCommand(1)
