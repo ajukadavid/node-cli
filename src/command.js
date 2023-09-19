056f6617a611877f7b1e566bd3ede7a7c403e3ea
ithiel883 <ajuka.zephiniah@gmail.com>
2023-09-19 11:04:11 +0100

    starting cli integration

--- src/command.js
+++ src/command.js
@@ -3,7 +3,10 @@ import { hideBin } from 'yargs/helpers'
 
 yargs(hideBin(process.argv))
     .command('new <note>', 'Create a new note', (yargs) => {}, () => {
-        
+        return yargs.positional('note', {
+          type: 'string',
+          description: 'The note to create'
+        })
     })    
 
   .demandCommand(1)
